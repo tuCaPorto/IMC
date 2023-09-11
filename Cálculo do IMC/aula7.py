263c706a8c0d0407093e073b246298900e496340
tuCaPorto <121845875+tuCaPorto@users.noreply.github.com>
2023-09-11 14:40:20 -0300

    Add files via upload

---
+++ Cálculo do IMC/aula7.py
@@ -0,0 +1,8 @@
+# Exercício de programação - Cálculo do IMC
+nome = 'Arthur Porto'
+peso = 93
+altura = 1.92
+imc = peso / (altura * altura)
+
+print("%s, tem %.2f de altura e pesa %d kgs" % (nome, altura,peso))
+print("Seu IMC é: ", imc)
